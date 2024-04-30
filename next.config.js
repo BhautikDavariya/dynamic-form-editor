be5dd4000780d24f27b6ed5850da8a6bec6dd429
bhautik davariya <bhautikmerndeveloprs@gmail.com>
2024-04-30 21:45:23 +0530

    update confing

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+  // Add basePath
+  basePath: '/dynamic-form-editor',
   experimental: {
     serverActions: true,
   },
